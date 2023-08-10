042b66780f1e82b12ba9038d8ac67b33f925775d
Deepanshu Singh <91874023+Deepanshuigtm@users.noreply.github.com>
2023-08-10 06:50:25 +0530

    Add files via upload
    Fixed data integration function

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin; // Note: The namespace should be in TitleCase
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator; // Import the Validator facade
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->passes()) {
+            if(Auth::guard('admin')->attempt(['email' => $request->email,'password' => $request->password],$request->get('remember'))){
+                $admin=Auth::guard('admin')->user();
+                if ($admin->role ==2){
+                    return redirect()->route('admin.dashboard');
+                }else{
+                    $admin=Auth::guard('admin')->logout();
+                    return redirect()->route('admin.login')->with('error','You are not autherised to use the admin pannel');
+                }
+            }else{
+                return redirect()->route('admin.login')->with('error','Either Email/Password is incorrect');
+            }
+        } else {
+            return redirect()->route("admin.login")
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
+    public function logout(){
+        $admin=Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
